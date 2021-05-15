528c7fbf8eb141da085fb315800995dc39973d9b
züleyha inal <zuleyhainl@outlook.com>
2021-05-15 19:15:34 +0300

    API (1)

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entitites.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //loosely coupled
+        IProductService _productService;
+        //IoC Container -- Inversion of Control
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            //dependency chain
+            var result = _productService.GetAll();
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.Get(id);
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("add")]//silme ve güncellemeler için de post kullanılabilir
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+    }
+}
